10bce6f13ca9bcb3da037607b7123365fc062536
SMARTSOFTDCS\oucema.zitoun <oucema.zitoun@gkfx.com>
2017-06-07 11:50:15 +0300

    Setting up test project in the solution.
    Linq1

---
+++ msdn-linq-samples/msdn-linq-samples-test/RestrictionOperatorsTests/RestrictionOperatorsTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace msdn_linq_samples_test.RestrictionOperatorsTests
+{
+    [TestClass]
+    public class RestrictionOperatorsTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ msdn-linq-samples/msdn-linq-samples-test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace msdn_linq_samples_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
