830370a937aac02f83c7418c537e3bc26e450435
SMARTSOFTDCS\oucema.zitoun <oucema.zitoun@gkfx.com>
2017-06-07 11:53:50 +0300

    Implementing the Program Main related to RestrictionOperators.
    Linq1

--- msdn-linq-samples/msdn-linq-samples/Program.cs
+++ msdn-linq-samples/msdn-linq-samples/Program.cs
@@ -1,15 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using msdn_linq_samples.LINQ;
 
 namespace msdn_linq_samples
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        public void RunRestrictionOperators()
         {
+            var restrictionOperators = new RestrictionOperators();
+
+            ConsoleKeyInfo cki;
+
+            do
+            {
+                restrictionOperators.DisplayMenu();
+                cki = Console.ReadKey(false);
+                switch (cki.KeyChar.ToString())
+                {
+                    case "1":
+                        restrictionOperators.Simple1();
+                        break;
+                    case "2":
+                        restrictionOperators.Simple2();
+                        break;
+                    case "3":
+                        restrictionOperators.Simple1();
+                        break;
+                    case "4":
+                        restrictionOperators.Simple2();
+                        break;
+                    case "5":
+                        restrictionOperators.Simple1();
+                        break;
+                    default:
+                        Console.WriteLine("No such choice in the list\n");
+                        break;
+                }
+            } while (cki.Key != ConsoleKey.Escape);
+        }
+
+        public void DisplayMenu()
+        {
+            Console.WriteLine("Program");
+            Console.WriteLine();
+            Console.WriteLine("1. Restriction Operators");
+            Console.WriteLine("Esc. Exit");
+        }
+
+        private static void Main()
+        {
+            var program = new Program();
+
+            ConsoleKeyInfo cki;
+
+            do
+            {
+                program.DisplayMenu();
+                cki = Console.ReadKey(false);
+                switch (cki.KeyChar.ToString())
+                {
+                    case "1":
+                        program.RunRestrictionOperators();
+                        break;
+                    default:
+                        Console.WriteLine("No such choice in the list\n");
+                        break;
+                }
+            } while (cki.Key != ConsoleKey.Escape);
+
         }
     }
 }
