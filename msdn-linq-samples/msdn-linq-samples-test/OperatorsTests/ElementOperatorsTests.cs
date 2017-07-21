d18064729b746639aab9d7c8c5b97e6d42e28d39
SMARTSOFTDCS\oucema.zitoun <oucema.zitoun@gkfx.com>
2017-07-21 11:52:37 +0300

    Implemented Simples / Wrote unit tests related to Element Operators.
    Linq-8

---
+++ msdn-linq-samples/msdn-linq-samples-test/OperatorsTests/ElementOperatorsTests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using msdn_linq_samples.LINQ;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace msdn_linq_samples_test.OperatorsTests
+{
+    [TestClass]
+    public class ElementOperatorsTests
+    {
+        private ElementOperators _elementOperators;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _elementOperators = new ElementOperators();
+        }
+
+        [TestMethod]
+        public void When_Linq58_Called_Should_Assert_true()
+        {
+            var result = _elementOperators.Simple58();
+            Assert.AreEqual(result.ProductName, "Chef Anton's Gumbo Mix");
+        }
+
+        [TestMethod]
+        public void When_Linq58_Called_Should_Assert_false()
+        {
+            var result = _elementOperators.Simple58();
+            Assert.AreNotEqual(result.ProductId, 1);
+        }
+
+        [TestMethod]
+        public void When_Linq59_Called_Should_Assert_true()
+        {
+            var result = _elementOperators.Simple59();
+            Assert.AreEqual(result, "one");
+        }
+
+        [TestMethod]
+        public void When_Linq59_Called_Should_Assert_false()
+        {
+            var result = _elementOperators.Simple59();
+            Assert.AreNotEqual(result, "zero");
+        }
+
+        [TestMethod]
+        public void When_Linq61_Called_Should_Assert_true()
+        {
+            var result = _elementOperators.Simple61();
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestMethod]
+        public void When_Linq61_Called_Should_Assert_false()
+        {
+            var result = _elementOperators.Simple61();
+            Assert.AreNotEqual(result, null);
+        }
+
+        [TestMethod]
+        public void When_Linq62_Called_Should_Assert_true()
+        {
+            var result = _elementOperators.Simple62();
+            Assert.AreEqual(result, null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException),"Should return null")]
+        public void When_Linq62_Called_Should_Assert_false()
+        {
+            var result = _elementOperators.Simple62();
+            Assert.AreNotEqual(result.ProductId, 1);
+        }
+
+        [TestMethod]
+        public void When_Linq64_Called_Should_Assert_true()
+        {
+            var result = _elementOperators.Simple64();
+            Assert.AreEqual(result, 8);
+        }
+
+        [TestMethod]
+        public void When_Linq64_Called_Should_Assert_false()
+        {
+            var result = _elementOperators.Simple64();
+            Assert.AreNotEqual(result, 4);
+        }
+    }
+}
