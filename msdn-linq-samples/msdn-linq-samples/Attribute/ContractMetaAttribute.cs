47c833fc172173e3f300066813e83a5828ab526a
SMARTSOFTDCS\oucema.zitoun <oucema.zitoun@gkfx.com>
2017-08-10 17:06:25 +0300

    Enhanced main menu's display.
    Linq-16

---
+++ msdn-linq-samples/msdn-linq-samples/Attribute/ContractMetaAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace msdn_linq_samples.Attribute
+{
+    public class ContractMetaAttribute : System.Attribute
+    {
+        public ContractMetaAttribute(string displayName)
+        {
+            DisplayName = displayName;
+        }
+
+        public string DisplayName { get; }
+    }
+}
---
+++ msdn-linq-samples/msdn-linq-samples/Display/EnumsDisplay.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using msdn_linq_samples.Attribute;
+using msdn_linq_samples.Provider;
+using msdn_linq_samples.Display;
+
+namespace msdn_linq_samples.Display
+{
+    public static class EnumsDisplay
+    {
+        public static void DisplayProgramMenu()
+        {
+            var programMenu = DataProvider.GetLinqOperators();
+            foreach (var item in programMenu)
+            {
+                Console.WriteLine(item.DisplayName());
+            }
+        }
+
+        public static string DisplayName(this Enum enumValue)
+        {
+            var declaredField = enumValue
+                .GetType()
+                .GetTypeInfo()
+                .GetDeclaredField(enumValue.ToString());
+
+            var contractMeta = declaredField == null ? null : declaredField.GetCustomAttribute<ContractMetaAttribute>();
+
+            return contractMeta != null ? contractMeta.DisplayName : enumValue.ToString();
+        }
+    }
+}
